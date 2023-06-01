c1f8c0e416ad0901419f488a8b8addcd54582a22
WilsonMachado <wilson.daniel12@gmail.com>
2023-06-01 10:51:58 -0500

    Se termina modal para manejar el error de usuario no logueado o registrado

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 
-export const Modal = () => {
+export const Modal = ({title, text, children}) => {
+    console.log(title);
+    console.log(text);
   return (
     <>
         <div className='modal'>
             <div className="modal-content">
                 <div className="modal-header">
-                    <h2>prop.title</h2>                   
+                    <h2>{title}</h2>                   
                 </div>
                 <div className="modal-text">
-                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Totam, repellendus excepturi laboriosam doloribus beatae nobis ipsam.</p>
+                    <p>{text}</p>
                 </div>
                 <div className="modal-options">
-                    <a href="#">prop.options</a>
-                    
+                    {children}                  
                 </div>
             </div>
         </div>
--- src/components/RouteWatchdog.jsx
+++ src/components/RouteWatchdog.jsx
@@ -1,12 +1,15 @@
-import { Navigate } from "react-router"
+import { Link } from "react-router-dom";
 import { Modal } from "./Modal";
 export const RouteWatchdog = ({children, currentUser, redirectTo="/bit02spa"}) => {
 
     /*< <Navigate to={redirectTo} /> */  
 
-    if(!currentUser){
+    if(!currentUser){   
         return <>
-            <Modal />                      
+            <Modal title={"Error"} text={"Antes de visitar su perfil, debe registarse o iniciar sesión."}>
+                <Link to={"/bit02spa/register"}>Register</Link>
+                <Link to={"/bit02spa/login"}>Login</Link>
+            </Modal>                      
         </>;
     }else{
         return children;
