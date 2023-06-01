d5cebca31e1008a57c8b50d2d1e07ff649b04763
WilsonMachado <wilson.daniel12@gmail.com>
2023-06-01 14:58:45 -0500

    Se crea arreglo de objetos para los items de la tienda

--- src/components/Pages/ShopPage.jsx
+++ src/components/Pages/ShopPage.jsx
@@ -5,15 +5,94 @@ export const ShopPage = () => {
   const products = [
     {
       id: 1,
-      name: "Extra Heart",
+      name: "Crossbow",
       description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure maiores reprehenderit nesciunt cupiditate sint autem distinctio!",
+      img: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/4/4f/Arrow_Loaded_Crossbow.png/",
       price: 10 
-    }
+    },
+    {
+      id: 2,
+      name: "Apple",
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure maiores reprehenderit nesciunt cupiditate sint autem distinctio!",
+      img: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/a/af/Apple_JE3_BE3.png/",
+      price: 10 
+    },
+    {
+      id: 3,
+      name: "Amethyst",
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure maiores reprehenderit nesciunt cupiditate sint autem distinctio!",
+      img: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/6/61/Amethyst_Shard_JE2_BE1.png",
+      price: 10 
+    },
+    {
+      id: 4,
+      name: "Arrow",
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure maiores reprehenderit nesciunt cupiditate sint autem distinctio!",
+      img: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/2/25/Arrow_%28item%29_JE1_BE1.png/",
+      price: 10 
+    },
+    {
+      id: 5,
+      name: "Bell",
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure maiores reprehenderit nesciunt cupiditate sint autem distinctio!",
+      img: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/8/8c/Bell_%28item%29_JE1_BE1.png/",
+      price: 10 
+    },
+    {
+      id: 6,
+      name: "Chainmail Helmet",
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure maiores reprehenderit nesciunt cupiditate sint autem distinctio!",
+      img: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/9/99/Chainmail_Helmet_%28item%29_JE2_BE2.png/",
+      price: 10 
+    },
+    {
+      id: 7,
+      name: "Chainmail Chestplate",
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure maiores reprehenderit nesciunt cupiditate sint autem distinctio!",
+      img: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/e8/Chainmail_Chestplate_%28item%29_JE2_BE2.png",
+      price: 10 
+    },
+    {
+      id: 8,
+      name: "Chainmail Leggings",
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure maiores reprehenderit nesciunt cupiditate sint autem distinctio!",
+      img: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/8/8b/Chainmail_Leggings_%28item%29_JE2_BE2.png/",
+      price: 10 
+    },
+    {
+      id: 9,
+      name: "Chainmail Boots",
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure maiores reprehenderit nesciunt cupiditate sint autem distinctio!",
+      img: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/8/8c/Chainmail_Boots_%28item%29_JE2_BE2.png/",
+      price: 10 
+    },
+    {
+      id: 10,
+      name: "Clock",
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure maiores reprehenderit nesciunt cupiditate sint autem distinctio!",
+      img: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/3/3e/Clock_JE3_BE3.gif/",
+      price: 10 
+    },
+    {
+      id: 11,
+      name: "Diamond",
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure maiores reprehenderit nesciunt cupiditate sint autem distinctio!",
+      img: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/a/ab/Diamond_JE3_BE3.png/",
+      price: 10 
+    },
+    {
+      id: 12,
+      name: "Eye of Ender",
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure maiores reprehenderit nesciunt cupiditate sint autem distinctio!",
+      img: "https://static.wikia.nocookie.net/minecraft_gamepedia/images/7/7a/Eye_of_Ender_JE2_BE2.png/",
+      price: 10 
+    },
   ];
 
   return (
     <div className='shop-container'>
       <h2>Our products</h2>
+      <img src="https://static.wikia.nocookie.net/minecraft_gamepedia/images/3/3e/Clock_JE3_BE3.gif/" alt="" />
     </div>
   )
 }
