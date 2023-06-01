6c6b12ea108d2ae9c9f57df542d472862f171fe4
WilsonMachado <wilson.daniel12@gmail.com>
2023-06-01 14:31:45 -0500

    Ahora los modals también tienen footer. Me equivoqué en el position

--- src/components/Pages/ShopPage.jsx
+++ src/components/Pages/ShopPage.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
 
 export const ShopPage = () => {
+
+  const products = [
+    {
+      id: 1,
+      name: "Extra Heart",
+      description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure maiores reprehenderit nesciunt cupiditate sint autem distinctio!",
+      price: 10 
+    }
+  ];
+
   return (
-    <div>ShopPage</div>
+    <div className='shop-container'>
+      <h2>Our products</h2>
+    </div>
   )
 }
