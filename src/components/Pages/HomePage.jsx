7184547f238a799ade914c0c9b66eb7d0e5ae0ae
WilsonMachado <wilson.daniel12@gmail.com>
2023-06-01 09:15:18 -0500

    Se realizan correcciones a la escritura de los textos

--- src/components/Pages/HomePage.jsx
+++ src/components/Pages/HomePage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const HomePage = () => {
   return (
       <>
-        <h1 className='main-title'>8-bit Shopping Store</h1>
+        <h1 className='main-title'>8-bit Store</h1>
         <img src="https://cdna.artstation.com/p/assets/images/images/053/325/198/original/lylian-leutard-shopkeeper-pres.gif?1661948452" alt="GIF" />  
       </>
     );
