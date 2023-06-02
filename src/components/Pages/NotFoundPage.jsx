da191c0625e7edcaba2f72bd7f211951fa608d80
WilsonMachado <wilson.daniel12@gmail.com>
2023-06-02 12:23:13 -0500

    Se cambia la vista del page not found por un modal. La imagen estaba muy fea

--- src/components/Pages/NotFoundPage.jsx
+++ src/components/Pages/NotFoundPage.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import { Modal } from '../Modal';
 
 export const NotFoundPage = () => {
-  return (
-    <div className="page-404-container"></div>
-  )
+  return (<Modal title={`Eror 404! Page not found`} text={"Apparently, the page you want to access does not exist. Try again!"}>
+              <Link to={"/bit02spa"}>Okay</Link>
+          </Modal>);
 }
