fe8299f0b1e131dbb5edf1ae6f27317bc335cde4
WilsonMachado <wilson.machado@colbits.com.co>
2023-06-02 02:04:18 -0500

    Ahora la cuenta se actualiza de forma dinámica con el carrito de compras

--- src/components/Pages/CartPage.jsx
+++ src/components/Pages/CartPage.jsx
@@ -1,82 +1,28 @@
-import React from 'react'
+import { useEffect } from 'react';
 import { ShoppingCartTable } from '../ShoppingCartTable'
 
-export const CartPage = () => {
+export const CartPage = ({cart, setCart}) => {
+
+    //** Ciclo de vida */
+
+    //? Montaje
+
+    useEffect(() => {
+        const currentCart = JSON.parse(localStorage.getItem('cart'));
+        if(currentCart !== null){
+          setCart(currentCart);
+        }
+      }, [])  
+  
+      //? Actualización 
+  
+      //? Desmontaje 
+
+
   return (
     <div className="cart-container">
         <h2>Bill</h2>
-        <ShoppingCartTable cartItems={[
-            {
-                "id": 7,
-                "quantity": 8,
-                "img": "https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/e8/Chainmail_Chestplate_%28item%29_JE2_BE2.png",
-                "name": "Chainmail Chestplate",
-                "price": 69.99
-            },
-            {
-                "id": 7,
-                "quantity": 1,
-                "img": "https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/e8/Chainmail_Chestplate_%28item%29_JE2_BE2.png",
-                "name": "Chainmail Chestplate",
-                "price": 69.99
-            },
-            {
-                "id": 7,
-                "quantity": 8,
-                "img": "https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/e8/Chainmail_Chestplate_%28item%29_JE2_BE2.png",
-                "name": "Chainmail Chestplate",
-                "price": 69.99
-            },
-            {
-                "id": 7,
-                "quantity": 1,
-                "img": "https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/e8/Chainmail_Chestplate_%28item%29_JE2_BE2.png",
-                "name": "Chainmail Chestplate",
-                "price": 69.99
-            },
-            {
-                "id": 7,
-                "quantity": 8,
-                "img": "https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/e8/Chainmail_Chestplate_%28item%29_JE2_BE2.png",
-                "name": "Chainmail Chestplate",
-                "price": 69.99
-            },
-            {
-                "id": 7,
-                "quantity": 1,
-                "img": "https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/e8/Chainmail_Chestplate_%28item%29_JE2_BE2.png",
-                "name": "Chainmail Chestplate",
-                "price": 69.99
-            },
-            {
-                "id": 7,
-                "quantity": 8,
-                "img": "https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/e8/Chainmail_Chestplate_%28item%29_JE2_BE2.png",
-                "name": "Chainmail Chestplate",
-                "price": 69.99
-            },
-            {
-                "id": 7,
-                "quantity": 1,
-                "img": "https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/e8/Chainmail_Chestplate_%28item%29_JE2_BE2.png",
-                "name": "Chainmail Chestplate",
-                "price": 69.99
-            },
-            {
-                "id": 7,
-                "quantity": 8,
-                "img": "https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/e8/Chainmail_Chestplate_%28item%29_JE2_BE2.png",
-                "name": "Chainmail Chestplate",
-                "price": 69.99
-            },
-            {
-                "id": 7,
-                "quantity": 1,
-                "img": "https://static.wikia.nocookie.net/minecraft_gamepedia/images/e/e8/Chainmail_Chestplate_%28item%29_JE2_BE2.png",
-                "name": "Chainmail Chestplate",
-                "price": 69.99
-            },
-        ]}>
+        <ShoppingCartTable cartItems={cart}>
             <button>Clear cart</button>
             <button>Checkout</button>
         </ShoppingCartTable>
